fa85a23d706f9fcaeca0bf3fe1cdde0d1de8d826
Brian Leett <b2lai@uwaterloo.ca>
2013-03-05 17:01:47 -0500

    Update jquery-databinding.js

--- jquery-databinding.js
+++ jquery-databinding.js
@@ -1,3 +1,23 @@
 (function ($) {
     /* some junk text about this script */
+    $.fn.dataBinding = function () {
+        var theThing = this;
+        
+        function update(endpoint, data, callback) {
+            if (endpoint && data) {
+                $.ajax( /* ... */);
+            }
+        }
+
+        return this.each(function () {
+            var $this = $(this);
+            $this.blur(function () {
+                var data = $this.data();
+
+                update(data.endpoint || '', 
+                       $this.val() || $this.text() || '', 
+                       data.callback || function () {});
+            });
+        });
+    };
 })(jQuery);
