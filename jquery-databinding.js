0897114e9d01fb0fb4a9110b4948936225255eae
Brian Leett <b2lai@uwaterloo.ca>
2013-03-05 17:07:12 -0500

    Update jquery-databinding.js

--- jquery-databinding.js
+++ jquery-databinding.js
@@ -1,22 +1,40 @@
 (function ($) {
     /* some junk text about this script */
     $.fn.dataBinding = function () {
-        var theThing = this;
+        var valueCache = [],
+            theThing = this;
         
-        function update(endpoint, data, callback) {
+        function updateDB(endpoint, data, callback) {
+            // upstream
             if (endpoint && data) {
-                $.ajax( /* ... */);
+                $.ajax({
+                    /* ... */
+                    success: callback
+                });
             }
+        }
+
+        function updateUI($elem) {
+            // downstream (initialisation)
+            $.ajax({
+                /* ... */
+                success: function (shittyData) {
+                    $elem.val(shittyData);
+                }
+            });
         }
 
         return this.each(function () {
             var $this = $(this);
+            
+            updateUI($this);
+
             $this.blur(function () {
                 var data = $this.data();
 
-                update(data.endpoint || '', 
-                       $this.val() || $this.text() || '', 
-                       data.callback || function () {});
+                updateDB(data.endpoint || '', 
+                         $this.val() || $this.text() || '', 
+                         data.callback || function () {});
             });
         });
     };
